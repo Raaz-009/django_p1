061a005d3c704c86bbd9bc9aa5f8bcc4e9d56731
Raaz-009 <thameemraz829@gmail.com>
2024-07-07 18:13:31 +0530

    upt

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+
 
 # Create your views here.
 def index(request):
@@ -16,5 +19,12 @@ def services(request):
     # return HttpResponse("this is service page")
 
 def contact(request):
+    if request.method =="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
     return render(request,'contact.html')
     #return HttpResponse("this is contact page")
